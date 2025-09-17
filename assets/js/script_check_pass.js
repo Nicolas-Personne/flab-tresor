66790c331625197a0f4b9550e6556c33e4989680
Nicolas-Personne <personne.nico@gmail.com>
2025-09-17 11:44:54 +0200

    Trim input value and ensure case-insensitive comparison for password check in script_check_pass.js

--- assets/js/script_check_pass.js
+++ assets/js/script_check_pass.js
@@ -4,9 +4,9 @@ const button = document.querySelector("#send-mdp");
 const error = document.querySelector("#error");
 if (button) {
 	button.addEventListener("click", () => {
-		let value = input.value;
+		let value = input.value.trim();
 
-		if (value.toLowerCase() === indices_info[0].response) {
+		if (value.toLowerCase() === indices_info[0].response.toLowerCase()) {
 			indice.classList.toggle("inactive");
 		} else {
 			error.textContent =
